1760dffc3b60ae80671ed8e0acf8166a6551cb9a
Peter Gyory <ptgyory@gmail.com>
2020-07-11 18:40:56 -0600

    scale text

--- src/TextManager.js
+++ src/TextManager.js
@@ -6,26 +6,34 @@ class TextManager {
 
     this.currentLine = 0;
     this.currentLetter = 0;
-    this.txtY = 210;
+    this.txtY = 300 / 780;
     this.currentLetter = 0;
-    this.leftX = 50;
-    this.highlightX = 50;
-    this.highlightY = -20;
+    this.LEFT_END = 150 / 780;
+    this.TEXT_SIZE = 20 / 780;
+    this.FADE_WINDOW = 300 / 780;
+    this.LINE_INCREMENT = 30 / 780;
+    this.TEXT_WIDTH = 12 / 780;
+    this.CUT_OFF = 340 / 780;
+    
+    
+    this.leftX = this.LEFT_END;
+    this.highlightX = this.LEFT_END;
+    this.highlightY = -this.TEXT_SIZE;
   }
 
   draw(ctx, canvasSize) {
     ctx.fillStyle = 'yellow';
-    ctx.fillRect(this.highlightX, this.txtY+this.highlightY, 11, 20);
-    ctx.font = "20px monospace";
+    ctx.fillRect(this.highlightX * canvasSize, (this.txtY + this.highlightY) * canvasSize, this.TEXT_WIDTH * canvasSize, this.TEXT_SIZE * canvasSize);
+    ctx.font = `${this.TEXT_SIZE * canvasSize}px monospace`;
     ctx.save();
     ctx.beginPath();
-    ctx.rect(0, 0, canvasSize, 250);
+    ctx.rect(0, 0, canvasSize, this.CUT_OFF * canvasSize);
     ctx.clip();
     for(let i = 0; i < this.text.length; i++){
-      var lineY = this.txtY+30*i;
-      var col = "rgba(0,0,0,"+(lineY/300)+")";
+      var lineY = this.txtY + this.LINE_INCREMENT * i;
+      var col = "rgba(0,0,0,"+ ( lineY / this.FADE_WINDOW)+")";
       ctx.fillStyle = col;
-      ctx.fillText(this.text[i], this.leftX, lineY);
+      ctx.fillText(this.text[i], this.leftX * canvasSize, lineY * canvasSize);
     }
     ctx.restore();
     // ctx.fillStyle = 'white';
@@ -45,15 +53,15 @@ class TextManager {
     var letter = this.text[this.currentLine][this.currentLetter].toLowerCase();
     if(key == letter){
       this.currentLetter++;
-      this.highlightX+=12;
+      this.highlightX += this.TEXT_WIDTH;
     }
     
     if(this.currentLetter == this.text[this.currentLine].length){
-      this.txtY -= 30;
+      this.txtY -= this.LINE_INCREMENT;
       this.currentLine++;
       this.currentLetter = 0;
       this.highlightX = this.leftX;
-      this.highlightY += 30;
+      this.highlightY += this.LINE_INCREMENT;
     }
   }
 }
